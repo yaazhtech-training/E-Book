6d3223e6d50723ff5bd145f93d310e81d2f7b778
Dhiva0426 <dhiva3078@gmail.com>
2024-10-17 04:29:08 -0400

    developer
    Responsive navbar completed

--- ebook/src/Components/Navbar.jsx
+++ ebook/src/Components/Navbar.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import { FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
+  const [ Open ,setOpen ] = useState(false);
+
+  const toggle = () => {
+      setOpen(!Open);
+  };
 
   return (
-    <header className="bg-[#66FCF1] text-[#1F2833] p-3">
+    <header className="bg-[#66FCF1] text-[#1F2833] p-5">
 
       <div className="container mx-auto flex justify-between items-center"> 
 
         <div className="flex text-center text-2xl font-bold basis-1/3">
-          <span className="text-[3rem]">Yaazh Books</span>
+          <span className="text-[2rem] md:text-[2.5rem] lg:text-[3rem]">Yaazh Books</span>
+        </div>
+
+        <div className="md:hidden" onClick={toggle}>
+          {Open ? <FaTimes size={28} /> : <FaBars size={28} />}
         </div>
 
         <nav className="hidden md:flex basis-2/3 font-bold justify-around items-center">
@@ -27,7 +37,19 @@ const Navbar = () => {
         </nav>
 
       </div> 
-
+      {Open && (
+        <nav className="md:hidden bg-blue-600 text-white p-3 mt-2">
+          <a href="/Home" className="block py-2 hover:text-gray-300">Home</a>
+          <a href="/shop" className="block py-2 hover:text-gray-300">Language</a>
+          <a href="/collections" className="block py-2 hover:text-gray-300">About us</a>
+          <a href="/contact" className="block py-2 hover:text-gray-300">Contact Us</a>
+          <button className="px-4 py-1 bg-[#52585f] text-white rounded-sm font-bold">
+            <a href="/Login" className="">Login</a>
+               <span> | </span>
+            <a href="/Signup" className="">Sign up</a>
+          </button>
+        </nav>
+      )} 
     </header>
   );
 };
